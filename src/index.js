45a5aff7b66a7c7b22e5f6de813316a4a62d3164
irynasmile <73856579+irynasmile@users.noreply.github.com>
2021-05-31 16:57:37 +0200

    instal hbs

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './sass/main.scss';
+
+import galleryItemTmp from './templates/gallery.hbs';
+import debounce from 'lodash.debounce';
+// const debounce = require('lodash.debounce');
+// const Handlebars = require('handlebars');
+// const template = Handlebars.compile('Name: {{name}}');
+// console.log(template({ name: 'Nils' }));
